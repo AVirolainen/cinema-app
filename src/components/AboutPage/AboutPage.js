934847e9f0bf758aa1ff5c309326b011faf030d9
AVirolainen <58146499+AVirolainen@users.noreply.github.com>
2021-10-20 11:27:04 +0300

    finishing front part

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,11 +1,11 @@
 import "./AboutPage.css"
 import movies from "./assets/movies.jpg"
 
-const AboutPage = ()=>{
-    return(
+const AboutPage = () => {
+    return (
         <div className="aboutPage">
             <div className="aboutImage">
-                <img src={movies} className="moviesImage"/>
+                <img src={movies} className="moviesImage" />
             </div>
             <div className="aboutText">
                 <div className="aboutHeader">
@@ -16,31 +16,35 @@ const AboutPage = ()=>{
                     <p>Kinolove – это самая большая сеть кинотеатров в Украине основанная в 2004 году.</p>
                     <p>Kinolove - это 28 кинотеатров и 141 кинозал в самых больших городах Украины.</p>
                     <p>Согласно лучшим мировым практикам корпоративного управления, в августе 2018 года был сформован высший орган управления компанией Kinolove – борд, в состав которого вошли влиятельные украинские предприниматели, управленцы и лидеры общественного мнения.
-                    Kinolove активно поддерживает и инвестирует в развитие украинского кинематографа.</p>
+                        Kinolove активно поддерживает и инвестирует в развитие украинского кинематографа.</p>
                 </div>
 
                 <div className="aboutBox">
-                    <div className="boxItem">
-                        <div className="boxImage">
-                            <img src="https://img.icons8.com/material-outlined/48/ffffff/collectibles.png"/>
+
+                    <div className="aboutBoxLine">
+                        <div className="boxItem">
+                            <div className="boxImage">
+                                <img src="https://img.icons8.com/material-outlined/48/ffffff/collectibles.png" />
+                            </div>
+                            <div className="boxText">
+                                Возврат билетов
+                            </div>
                         </div>
-                        <div className="boxText">
-                            Возврат билетов
+
+                        <div className="boxItem">
+                            <div className="boxImage">
+                                <img src="https://img.icons8.com/material-outlined/48/ffffff/bonds.png" />
+                            </div>
+                            <div className="boxText">
+                                Карьера
+                            </div>
                         </div>
                     </div>
 
+                    <div className="aboutBoxLine">
                     <div className="boxItem">
                         <div className="boxImage">
-                            <img src="https://img.icons8.com/material-outlined/48/ffffff/bonds.png"/>
-                        </div>
-                        <div className="boxText">
-                            Карьера
-                        </div>
-                    </div>
-
-                    <div className="boxItem">
-                        <div className="boxImage">
-                            <img src="https://img.icons8.com/material-outlined/48/ffffff/collectibles.png"/>
+                            <img src="https://img.icons8.com/material-outlined/48/ffffff/collectibles.png" />
                         </div>
                         <div className="boxText">
                             Поддержка
@@ -49,18 +53,18 @@ const AboutPage = ()=>{
 
                     <div className="boxItem">
                         <div className="boxImage">
-                            <img src="https://img.icons8.com/material-outlined/48/ffffff/dividends.png"/>
+                            <img src="https://img.icons8.com/material-outlined/48/ffffff/dividends.png" />
                         </div>
                         <div className="boxText">
                             Реклама
                         </div>
+                    </div>
+
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
-// test 2
 
 export default AboutPage
